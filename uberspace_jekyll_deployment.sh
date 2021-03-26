8750cd6f5deba883734cef6485028665392a9f01
Florian Latzel <florian.latzel@gmail.com>
2021-03-27 00:57:44 +0100

    Removed post-receive parts and streamlined cli call.

--- uberspace_jekyll_deployment.sh
+++ uberspace_jekyll_deployment.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-# Deployment of Jekyll-Sites on Uberspace 
-# via Git Bare Repository and post-receive Hook.
+# Manual deployment of jekyll sites 
 #
 # See https://netzaffe.de/jekyll-deployment-auf-uberspace-via-bare-repo-und-post-receive-hook/ 
 # for requirements and more detailed description (german)
-
-if [[ $# -eq 1 && $1 == "--cli" ]] 
-then
-	pushed_branch="NONE" 
-else
-	read oldrev newrev ref 
-	pushed_branch=${ref#refs/heads/} 
-fi
 
 ## Variables
 
@@ -32,7 +23,6 @@ www=/var/www/virtual/${USER}/${subdomain}${domain}
 
 ## Do the magic
  
-[[ "$pushed_branch" != "$build_branch" && "$pushed_branch" != "NONE" ]] && exit
 tmp=$(mktemp -d)
 git clone ${git_repo} ${tmp}
 cd ${tmp}
